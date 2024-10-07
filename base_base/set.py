e8706a822e078ab711d91a8172beaa644359eff2
jukki <jukki.snow@gmail.com>
2024-10-08 06:37:16 +0700

    Продолжение

--- base_base/set.py
+++ base_base/set.py
@@ -1,3 +1,19 @@
 a_set = {1, 'alex', 3.14}
+
 print(a_set)
 print(type(a_set))
+
+# Добавление элементов в множество
+a_set.add('Jukki')
+print(a_set)
+
+# Добавление нескольких элементов в множество
+a_set.update(['and', 21, 88])
+print(a_set)
+
+# Удаление элементов из множества
+a_set.remove(1)
+print(a_set)
+
+a_set.discard(88)
+print(a_set)
