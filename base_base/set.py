a2f8d45809ac3424dcd89c650e628da5bc8aacc7
jukki <jukki.snow@gmail.com>
2024-10-09 06:47:44 +0700

    Множества

--- base_base/set.py
+++ base_base/set.py
@@ -17,3 +17,25 @@ print(a_set)
 
 a_set.discard(88)
 print(a_set)
+
+a_set.pop()
+print(a_set, '\n')
+
+# print(dir(set), '\n')
+
+# Функция вхождения во множество
+one_set = {1, 2, 3, 4, 5}
+two_set = {5, 6, 7, 8, 9}
+print(7 in one_set)
+print(7 in two_set)
+print(len(one_set), '\n')
+
+# Очистка множества
+two_set.clear()
+print(two_set)
+
+two_set.update([5, 6, 7, 8, 9])
+print(two_set)
+
+# del two_set
+# print(two_set)
