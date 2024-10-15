8788a683f55d114197b3108f78be23c04e089f23
jukki <jukki.snow@gmail.com>
2024-10-15 20:05:15 +0700

    Продолжение со списками(финал)

--- base_base/list.py
+++ base_base/list.py
@@ -21,3 +21,32 @@ a = b_list[:5]
 b = b_list[5:]
 c = b_list[:10:2]
 print(a, b, c)
+
+# Операции со списками
+print(len(b_list))
+print(min(b_list))
+print(max(b_list))
+c_list = a_list + b_list + ["fuck you!"]
+print(c_list, '\n')
+
+# Методы списков
+удаляет_последний_элемент = b_list.pop()
+print(удаляет_последний_элемент)
+print(b_list)
+
+удаление_по_индексу = b_list.pop(0)
+print(удаление_по_индексу)
+del a_list[0]
+a_list.remove('pens')
+print(a_list)
+
+
+b_list.append(10)
+print(b_list)
+b_list.extend([5, 5, 1, 0.5, 2.5])
+print(b_list)
+print(sorted(b_list))
+b_list.sort(reverse=True)
+print(b_list)
+
+print(b_list.count(5))
