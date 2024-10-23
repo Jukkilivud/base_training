f44cf314a3fc31d6d4d476baf48c9d72ec753523
jukki <jukki.snow@gmail.com>
2024-10-23 09:00:57 +0700

    Треня по строкам

--- base_base/string.py
+++ base_base/string.py
@@ -1,9 +1,26 @@
-a = 1
-print(a)
+a_string = 'Alex'
+a_string_2 = 'Jukki'
+print(a_string + ' ' + a_string_2)
+print('fuck ' * 3)
+print(len('fuck'))
+
+# Доступ по индексу
+s = a_string
+print(s[0])
+print(s[-1])
+
+# Срез
+x = a_string_2
+print(x[:])
+print(x[0:1])
+print(x[2:4])
+print(x[::2])
+print(x[::-2])
+print(x[:2])
+print(x[2:], '\n')
 
 
-def test():
-    return print('hi')
-
-
-test()
+word = a_string.isalpha()
+print(word)
+word_2 = 'Alex'.isalpha()
+print(word_2)
