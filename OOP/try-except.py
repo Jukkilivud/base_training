39627fb24bd9dca87a4a719e1ee768fc7c1e860a
jukki <jukki.snow@gmail.com>
2024-10-30 06:26:00 +0700

    Исключения try-except-else-finally

---
+++ OOP/try-except.py
@@ -0,0 +1,51 @@
+# -*-coding:cp1251-*-
+
+print("Program started")
+try:
+    print("Opening file...")
+    f = open("README.tt", 'r')
+    print(*f)
+except:
+    print("Данный файл не найден!" + " File not found!")
+finally:
+    x = open("README.txt", 'r')
+    print(*x)
+    x.close()
+print("Program finished", '\n')
+
+
+print("Program started")
+try:
+    print("Opening file...")
+    f = open("README.tt", 'r')
+    print(*f)
+except FileNotFoundError:
+    print("Данный файл не найден!" + " File not found!")
+except Exception:
+    print("Что-то пошло не так..." + " Something gone wrong...")
+finally:
+    x = open("README.txt", 'r')
+    print(*x)
+    x.close()
+print("Program finished", '\n')
+
+
+print("Program started")
+try:
+    print("Opening file...")
+    f = open("README.txt", 'r+')
+    print(*f)
+    try:
+        print("Запись в файл" + " Writing to file...")
+        f.write("  Нужно же что-то добавить :))")
+    except Exception:
+        print("Что-то пошло не так..." + " Something gone wrong...")
+    else:
+        print("Успешно" + ' Success')
+except FileNotFoundError:
+    print("Данный файл не найден!" + " File not found!")
+finally:
+    x = open("README.txt")
+    print(*x)
+    x.close()
+print("Program finished")
